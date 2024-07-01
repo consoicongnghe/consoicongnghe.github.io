d818671464e717f5ce40329b19b1a6682953b4d2
consoicongnghe <consoicongnghe@gmail.com>
2024-07-01 13:47:55 +0700

    defpss

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function showHiddenString1() {
+	var x = document.getElementById("hiddenString1");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+} 
