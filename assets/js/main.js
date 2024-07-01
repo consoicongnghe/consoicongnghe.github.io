e178ab5b7110eb185f9bd418765b0328e7f6321f
consoicongnghe <consoicongnghe@gmail.com>
2024-07-01 16:25:28 +0700

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,23 @@ function showHiddenString1() {
 	} else {
 		x.style.display = "none";
 	}
+}
+
+// https://www.w3schools.com/howto/howto_js_sticky_header.asp
+// When the user scrolls the page, execute stickytopnav
+window.onscroll = function () { stickytopnav() };
+
+// Get the header
+var header = document.getElementById("topnav");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickytopnav() {
+	if (window.scrollY > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
 } 
