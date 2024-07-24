5fb8747d9017e967190b00b52221ee6d3716cf56
consoicongnghe <consoicongnghe@gmail.com>
2024-07-24 12:49:30 +0700

    improve navbar, display text on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// press the button to show hidden string
 function showHiddenString1() {
 	var x = document.getElementById("hiddenString1");
 	if (x.style.display === "none") {
@@ -10,10 +11,16 @@ function showHiddenString1() {
 // https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 // Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
 function topnavHamburger() {
-	var x = document.getElementById("topnav");
-	if (x.className === "topnav") {
+	var x = document.getElementById("topnav-right");
+	if (x.className === "topnav-right") {
 		x.className += " responsive";
 	} else {
-		x.className = "topnav";
+		x.className = "topnav-right";
 	}
+	// var x = document.getElementById("topnav-hamburger");
+	// if (x.className === "topnav-hamburger") {
+	// 	x.className += " active";
+	// } else {
+	// 	x.className = "topnav-hamburger";
+	// }
 }
